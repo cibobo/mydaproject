eba487d33e84ce8c8fab5cc03124b4764ac5bbfd
cibobo2005@googlemail.com <cibobo2005@googlemail.com@b99864b5-48d0-320f-a9dc-3252b97c2a7e>
2011-11-15 17:16:49 +0000

    With bug in TestOpenGL.cpp

--- trunk/TestOpenGL/TestOpenGL/TestOpenGL.h
+++ trunk/TestOpenGL/TestOpenGL/TestOpenGL.h
@@ -1,16 +1,20 @@
 #pragma once
 
+#include "process.h" 
 #include "resource.h"
+#include "stdio.h" 
+#include "windows.h" 
 
-#include "TestOpenGL.h"
+
+#include "ARToolKit.hpp"
+#include "DistanceFilter.hpp"
+#include "ImageProcess.hpp"
+#include "MarkerDetection.hpp"
 #include "OpenGLWin.hpp"
 #include "PMDCamIO.hpp"
-#include "ARToolKit.hpp"
-#include "MarkerDetection.hpp"
+#include "TestOpenGL.h"
+
 
 #include <cv.h>
 #include <highgui.h>
 
-#include "stdio.h" 
-#include "windows.h" 
-#include "process.h" 
